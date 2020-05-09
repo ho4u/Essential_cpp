5049f6c31a39abb2220a09e16bd44e7c18595921
ho4u <kulikov87dev@gmail.com>
2020-05-09 19:26:28 +0300

    add solution to ex. 1.5

---
+++ ch_1/ex_1.5.cpp
@@ -0,0 +1,36 @@
+// Write a program to ask the user his or her name. 
+// Read the response. Confirm that the input is at least two
+// characters in length. If the name seems valid, respond to the user.
+// Provide two implementations: one using a C-style character string,
+// and the other using a string class object.
+
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+
+using namespace std;
+
+/*int main()
+{
+    const size_t name_sz = 20;
+    char usr_name[name_sz];
+    cout << "Enter user name: ";
+    cin >> setw(name_sz) >> usr_name;
+    if (strlen(usr_name) > 1)
+        cout << "Your name is " << usr_name << endl;
+    else
+        cout << "Your name is too short" << endl;
+    return 0; 
+}*/
+
+int main()
+{
+    string usr_name;
+    cout << "Enter user name: ";
+    cin >> usr_name;
+    if (usr_name.size() > 1)
+        cout << "Your name is " << usr_name << endl;
+    else
+        cout << "Your name is too short" << endl;
+    return 0; 
+}
